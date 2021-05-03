8affa8e78e8e62a25f584a52be53a5546266ca92
Roman Rosluk <roman.rosluk@utopix.ch>
2021-05-03 12:15:35 +0200

    Master router: Show MasterRouteConfig

--- lib/master_router.dart
+++ lib/master_router.dart
@@ -3,6 +3,7 @@ library master_router;
 export 'src/main.dart'
     show
         MasterRoutePredicate,
+        MasterRouteConfig,
         MasterRouteTransition,
         MasterRoute,
         MasterRouteParamsAbstract,
